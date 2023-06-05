7a1563b3ad0536276167de271cda45178e0dd3ec
Yudhistira Tribuana Authar <yudhistira.tribuana@gmail.com>
2023-06-05 10:32:39 +0700

    Installed socket.io

--- index.js
+++ index.js
@@ -5,13 +5,22 @@ const port = process.env.PORT || 3000;
 const cors = require("cors");
 const router = require("./routes");
 const errorHandling = require("./middlewares/errorHandling.js");
+const http = require('http');
+
+
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(router);
 app.use(errorHandling);
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+io.on('connection', (socket) => {
+  console.log('a user connected');
+}); 
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+server.listen(3000, () => {
+  console.log('listening on *:3000');
 });
